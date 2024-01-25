62716997cad532ce7d45dee93d323002dd2a067c
wirayuda <wirayuda233@gmail.com>
2024-01-26 01:55:48 +0800

    update array tags

--- hipnode/app/(root)/page.tsx
+++ hipnode/app/(root)/page.tsx
@@ -10,9 +10,23 @@ import {
 import { FILTER_ITEMS } from "@/constants/home";
 import { getAllPosts } from "@/lib/actions/post.action";
 import PodcastCard from "@/components/shared/podcast-card";
+import { Tag } from "@/types";
+import result from "postcss/lib/result";
 const PopularTags = dynamic(() => import("@/components/shared/popular-tags"));
 const Loader = dynamic(() => import("@/components/shared/Loader"));
 
+const uniqueFlatTags = (tagArrays: Tag[][]): Tag[] => {
+  const uniqueNames: Set<string> = new Set();
+  return tagArrays
+    .flat() // Flatten the array
+    .filter((tag) => {
+      if (!uniqueNames.has(tag.name.toLowerCase())) {
+        uniqueNames.add(tag.name);
+        return true;
+      }
+      return false;
+    });
+};
 export default async function Home({
   searchParams,
 }: {
@@ -20,7 +34,9 @@ export default async function Home({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const { posts } = await getAllPosts(searchParams.sort as string, 1, 10);
-  const tags = posts.map((post) => post.tags).flat(2);
+  const tags = posts.map((post) => post.tags);
+  const result = uniqueFlatTags(tags);
+  console.log(result);
 
   return (
     <section className="flex flex-col gap-3 py-5 lg:flex-row">
@@ -32,11 +48,8 @@ export default async function Home({
           titleStyles="hidden sm:block"
         />
         <Suspense fallback="Loading....">
-          {tags.length >= 1 && (
-            <PopularTags
-              items={Array.from(new Set(tags))}
-              styles="hidden md:block"
-            />
+          {result?.length >= 1 && (
+            <PopularTags items={result} styles="hidden md:block" />
           )}
         </Suspense>
       </div>
@@ -50,7 +63,7 @@ export default async function Home({
         <Suspense fallback="Loading...">
           {tags.length >= 1 && (
             <PopularTags
-              items={Array.from(new Set(tags))}
+              items={result}
               styles="block md:hidden"
               innerStyles="justify-start items-start"
             />
