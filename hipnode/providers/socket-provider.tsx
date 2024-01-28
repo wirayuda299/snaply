672a31d3331aff8b724e670015bba5de24224fc1
wirayuda <wirayuda233@gmail.com>
2024-01-28 17:41:36 +0800

    add env

--- hipnode/providers/socket-provider.tsx
+++ hipnode/providers/socket-provider.tsx
@@ -33,7 +33,8 @@ export const SocketProvider: FC<ContextProviderProps> = ({ children }) => {
 	useEffect(() => {
 		const connectSocket = async () => {
 			try {
-				await fetch('/api/socket');
+				const serverUrl = process.env.NEXT_PUBLIC_SITE_URL;
+				await fetch(`${serverUrl}/api/socket`);
 				const socketConnection = io({
 					addTrailingSlash: false,
 				});
