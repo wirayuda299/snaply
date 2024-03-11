f0b2c2becd465ce93f8d6f86efffcd885d19fdc1
wirayuda <wirayuda233@gmail.com>
2024-03-12 03:08:37 +0800

    update config redis

--- backend/src/config/redis.config.ts
+++ backend/src/config/redis.config.ts
@@ -5,11 +5,7 @@ dotenv.config();
 export const client =
 	process.env.NODE_ENV === 'production'
 		? createClient({
-				password: 'ldYRgzwcEP4736yi6dmFZbHas81Y6RIC',
-				socket: {
-					host: 'redis-15920.c322.us-east-1-2.ec2.cloud.redislabs.com',
-					port: 15920,
-				},
+				url: 'rediss://default:f651f52905834b8e9e2015b1cec7b812@engaged-liger-45328.upstash.io:45328',
 		  })
 		: createClient();
 
@@ -25,9 +21,14 @@ export async function connectRedis() {
 	try {
 		await client
 			.on('error', (error) => {
+				console.log(error);
+
 				throw error;
 			})
-			.connect();
+			.connect()
+			.then((res) => {
+				console.log('connected to redis -> ', res.ACL_USERS);
+			});
 	} catch (error) {
 		throw error;
 	}
